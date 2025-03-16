6ab0a74ee510528e65667e2d5e47015137c56de3
parth-faldu <parth123faldu@gmail.com>
2025-03-16 11:14:34 +0530

    Add CORS_ORIGIN to environment variables and update server CORS configuration

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
 };
